731c1c1f638f4e536fef8724c56e6e66993a4acd
Yevhen02 <153821695+Yevhen02@users.noreply.github.com>
2023-12-27 09:20:01 +0200

    usage of for...of loop

--- js/task-2.js
+++ js/task-2.js
@@ -1,16 +1,33 @@
+// МЕТОД SLICE() 
+// function makeArray(firstArray, secondArray, maxLength) {
+//   let newArray = firstArray.concat(secondArray);
+//   if (newArray.length > maxLength) {
+//     newArray = newArray.slice(0, maxLength);
+//   }
+//   return newArray;
+// }
+
+// ЦИКЛ FOR...OF
 function makeArray(firstArray, secondArray, maxLength) {
-  let newArray = firstArray.concat(secondArray);
-  if (newArray.length > maxLength) {
-    newArray = newArray.slice(0, maxLength);
+  let newArray = [];
+  for (let item of firstArray) {
+    newArray.push(item);
   }
-  return newArray;
+  for (let item of secondArray) {
+    newArray.push(item);
+  }
+  if (newArray.length > maxLength) {
+    return newArray.slice(0, maxLength);
+  } else {
+    return newArray;
+  }
 }
 
-console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3));
-console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4));
-console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3));
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2));
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4));
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
 
 console.log("..................");
--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,28 @@
+// ЦИКЛ - FOR
+// function filterArray(numbers, value) {
+//   let newArray = [];
+//   for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] > value) {
+//       newArray.push(numbers[i]);
+//     }
+//   }
+//   return newArray;
+// }
+
+// ЦИКЛ - FOR...OF
 function filterArray(numbers, value) {
-  let newArray = [];
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] > value) {
-      newArray.push(numbers[i]);
+  let filteredArray = [];
+
+  for (let number of numbers) {
+    if (number > value) {
+      filteredArray.push(number);
     }
   }
-  return newArray;
+
+  return filteredArray;
 }
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); 
-console.log(filterArray([1, 2, 3, 4, 5], 4)); 
-console.log(filterArray([1, 2, 3, 4, 5], 5)); 
-console.log(filterArray([12, 24, 8, 41, 76], 38)); 
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
